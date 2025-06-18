15379ee33732a04efacaba926897dcaa6d680c2a
himchan92 <omea92@naver.com>
2025-06-18 12:53:14 +0900

    JPA 엔티티 컬럼속성 정리

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,16 +1,37 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id //PK 설정
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
 
-    protected Member() {}
+    private Integer age;
+
+    //주의: 반드시 STRING 타입 해야지 값변경안되게 보장됨
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    //자바 8이후 되는 JPA 아래 날짜함수 지원하여 그대로 사용하면됨
+    private LocalDate createdDate;
+    private LocalDateTime lastModifiedDate;
+
+    @Lob //대량사이즈
+    private String description;
+
+    @Transient
+    private int temp;
+
+    public Member() {}
 
     public Member(Long id, String name) {
         this.id = id;
@@ -31,5 +52,53 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
     }
 }
