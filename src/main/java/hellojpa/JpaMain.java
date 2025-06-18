9ff3652066288424c54f08b2a63d940cbb483172
himchan92 <omea92@naver.com>
2025-06-18 12:07:06 +0900

    JPA persist find setter 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,18 +16,18 @@ public class JpaMain {
 
         // 스프링은 try~catch~finally 처리지원
         try {
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("HelloA");
-            em.persist(member); //영속성컨택스트 등록
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("HelloA");
+//            em.persist(member); //영속성컨택스트 등록
 
-//            Member findMember = em.find(Member.class, 1L); //JPA PK기준 조회
+            Member findMember = em.find(Member.class, 1L); //JPA PK기준 조회
 //            findMember.setName("HelloJPA"); //변경감지 JPA UPDATE 수행지원
 
             //JPQL : Entity 대상 수행
             List<Member> result = em.createQuery("select m from Member")
-                    .setFirstResult(5) //페이징 첫
-                    .setMaxResults(8) //페이징 마지막
+                    .setFirstResult(0) //페이징 첫
+                    .setMaxResults(1) //페이징 마지막
                     .getResultList();
 
             for(Member m : result) {
