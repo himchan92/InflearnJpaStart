1d4f7f81d749d358fe70b28179edab03379d189e
himchan92 <omea92@naver.com>
2025-06-18 12:23:55 +0900

    JPA 영속성컨택스트 개념실습2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,7 +12,7 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
-        tx.begin();
+        tx.begin(); //변경작업시 반드시 필요
 
         // 스프링은 try~catch~finally 처리지원
         try {
@@ -22,9 +22,22 @@ public class JpaMain {
             member.setName("HelloJPA");
 
             //영속
-            System.out.println("BEFORE");
-            em.persist(member); // INSERT 비슷 영속성 등록
-            System.out.println("AFTER");
+            Member member1 = new Member(1L, "A");
+            Member member2 = new Member(2L, "B");
+            em.persist(member1);
+            em.persist(member2);
+            System.out.println("-------------------------------------------");
+
+            //1차캐시에서 먼저조회 후 없으면 그때 DB에서 조회
+            Member findMember = em.find(Member.class, 1L);
+            Member findMember2 = em.find(Member.class, 1L);
+
+            //1차캐시로 조회 시 영속성 엔티티 동일성 보장
+            System.out.println("result = " + (findMember == findMember2));
+
+            //조회 내역 setter 시 변경감지 발생하여 별도 UPDATE 호출없이 JPA가 UPDATE 수행지원
+            Member member3 = em.find(Member.class, 1L);
+            member3.setName("zzzzzz");
 
             tx.commit(); // 실제 DB 반영처리
         } catch (Exception e) {
