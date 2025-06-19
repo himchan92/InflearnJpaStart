bd01a4956d835e543a859fd9db3d5d5483d6a1f0
himchan92 <omea92@naver.com>
2025-06-19 12:22:42 +0900

    JPA 연관관계 매핑 실습1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,30 +12,30 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin(); //변경작업시 반드시 필요
 
-        // 스프링은 try~catch~finally 처리지원
         try {
-            //비영속
+
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
             Member_ex memberEx = new Member_ex();
-            memberEx.setId(1L);
-            memberEx.setName("HelloJPA");
+            memberEx.setUsername("member1");
+            //memberEx.setTeamId(team.getId());
+            memberEx.setTeam(team); //왜래키 TEAM 참조
+            em.persist(memberEx);
 
-            //영속
-            Member_ex memberEx1 = new Member_ex(1L, "A");
-            Member_ex memberEx2 = new Member_ex(2L, "B");
-            em.persist(memberEx1);
-            em.persist(memberEx2);
-            System.out.println("-------------------------------------------");
+            //영속성 초기화클리어
+            em.flush();
+            em.clear();
 
-            //1차캐시에서 먼저조회 후 없으면 그때 DB에서 조회
-            Member_ex findMemberEx = em.find(Member_ex.class, 1L);
-            Member_ex findMemberEx2 = em.find(Member_ex.class, 1L);
+            //조회
+            Member_ex findMember = em.find(Member_ex.class, memberEx.getId());
 
-            //1차캐시로 조회 시 영속성 엔티티 동일성 보장
-            System.out.println("result = " + (findMemberEx == findMemberEx2));
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam);
 
-            //조회 내역 setter 시 변경감지 발생하여 별도 UPDATE 호출없이 JPA가 UPDATE 수행지원
-            Member_ex memberEx3 = em.find(Member_ex.class, 1L);
-            memberEx3.setName("zzzzzz");
+            Team newTeam = em.find(Team.class, 1L);
+            findMember.setTeam(newTeam);
 
             tx.commit(); // 실제 DB 반영처리
         } catch (Exception e) {
--- src/main/java/hellojpa/Member_ex.java
+++ src/main/java/hellojpa/Member_ex.java
@@ -9,34 +9,19 @@ import java.time.LocalDateTime;
 public class Member_ex {
 
     @Id //PK 설정
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) //MySQL 타입 auto increment 지원
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false)
-    private String name;
+    @Column(name = "USERNAME")
+    private String username;
 
-    private Integer age;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
 
-    //주의: 반드시 STRING 타입 해야지 값변경안되게 보장됨
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    //자바 8이후 되는 JPA 아래 날짜함수 지원하여 그대로 사용하면됨
-    private LocalDate createdDate;
-    private LocalDateTime lastModifiedDate;
-
-    @Lob //대량사이즈
-    private String description;
-
-    @Transient
-    private int temp;
-
-    public Member_ex() {}
-
-    public Member_ex(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    @ManyToOne //다대1 <->1대다
+    @JoinColumn(name = "TEAM_ID") //Team > team_id 조인
+    private Team team;
 
     public Long getId() {
         return id;
@@ -46,59 +31,27 @@ public class Member_ex {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public Integer getAge() {
-        return age;
+//    public Long getTeamId() {
+//        return teamId;
+//    }
+//
+//    public void setTeamId(Long teamId) {
+//        this.teamId = teamId;
+//    }
+
+    public Team getTeam() {
+        return team;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public LocalDate getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDate createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public LocalDateTime getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getTemp() {
-        return temp;
-    }
-
-    public void setTemp(int temp) {
-        this.temp = temp;
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
