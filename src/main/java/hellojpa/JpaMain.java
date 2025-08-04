51c6282e0b20cf4af3ff157586f266565889b80a
himchan92 <omea92@naver.com>
2025-08-04 12:00:46 +0900

    복습 init commit

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,7 +8,6 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
         em.close();
         emf.close();
