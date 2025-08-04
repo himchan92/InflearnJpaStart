b0bafe91d1c32b720a8396dce99602a3828ce38f
himchan92 <omea92@naver.com>
2025-08-04 12:58:22 +0900

    복습 JPA 영속성 상태별 정리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,16 +15,27 @@ public class JpaMain {
         tx.begin(); //트랜젝션 시작
 
         try {
-            //Member findMember = em.find(Member.class, 1L);
-            //findMember.setName("HelloJPA"); //JPA SETTER 시 변경감지로 UPDATE 수행
-            List<Member> result = em.createQuery("select m from Member as m", Member.class) //엔티티명 대상 수행
-                    .setFirstResult(1) //페이징 1 시작
-                    .setMaxResults(8) //페이징 8 종료
-                    .getResultList(); //다건조회
+            //비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
 
-            for(Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+            //영속
+            em.persist(member);
+
+            //1차 캐시조회 : DB 조회전 조회하여 있으면 대신 반환하고 없으면 DB쪽 조회하여 성능개선
+            //1차캐시 있으면 동일성 보장
+            Member findMember1 = em.find(Member.class, "member1");
+            Member findMember2 = em.find(Member.class, "member1");
+
+            //엔티티 수정 : setter처리시 변경감지 일어나 UPDATE 수행
+            member.setName("HelloMyBatis");
+
+            //영속성 클리어 : 캐시 제거되어 이후 find 시 캐시없으니 쿼리 호출됨
+            em.clear();
+
+            //영속 분리 하여 준영속
+            //em.detach(member);
 
             tx.commit(); //DB 실제 반영
         } catch (Exception e) {
