dc603602e8be66cb85a388e8ba51f8de552ae25c
himchan92 <omea92@naver.com>
2025-08-21 10:58:40 +0900

    엔티티 실습1 진행중(Member~)

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+}
