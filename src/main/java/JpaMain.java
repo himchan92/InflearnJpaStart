697f5ea825ee6bb8f2e222e9af6760a6664ed4bd
omea92 <thdjun92@naver.com>
2025-08-25 00:30:38 +0900

    jpa JPQL 복습

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -1,7 +1,9 @@
+import hellojpa.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -13,6 +15,15 @@ public class JpaMain {
       tx.begin();
 
       try {
+
+        List<Member> resultList = em.createQuery("select m from Member m where m.username like '%kim%'", Member.class)
+            //페이징 0 ~ 10
+            .setFirstResult(0)
+            .setMaxResults(10)
+            .getResultList();
+
+        System.out.println(resultList);
+
         tx.commit();
       } catch (Exception e) {
         tx.rollback();
