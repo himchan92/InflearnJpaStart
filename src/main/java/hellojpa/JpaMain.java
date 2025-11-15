e3286e595285fa13f0d8e557abe523b1b2802080
omea92 <thdjun92@naver.com>
2025-11-15 22:36:06 +0900

    member entity

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -9,7 +9,24 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
 
-        em.close();
+        // TODO: JPA 모든 변경은 트랜젝션안에서 이뤄져야해서 필수 셋팅
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA"); //별도 저장로직없이 Transaction 안에서는 변경있을시 자동 UPDATE 수행지원
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
