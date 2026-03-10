59c328dc03c3be61b82b904e35129bfb2107b167
himchan92 <omea92@naver.com>
2026-03-10 13:06:26 +0900

    jpa setting

--- src/main/java/jpabook/jpashop/domain/Team.java
+++ src/main/java/jpabook/jpashop/domain/Team.java
@@ -1,9 +1,9 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -13,4 +13,13 @@ public class Team {
     @Column(name = "team_id")
     private Long id;
     private String name;
+
+    /*
+        mappedBy
+        - 반대편에 team 매핑 걸려있는걸 명시
+        - 연관관계 주인 아닌쪽에 명시
+        - 주인 아닌쪽은 읽기만 가능
+     */
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
 }
