7a15ad77f2e572260678ce8ef6bef00ea19e3974
omea92 <thdjun92@naver.com>
2026-04-12 22:47:15 +0900

    init

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -12,24 +12,7 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
 
-        // jpa 모든작업은 트랜젝션내에서 해야함
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-
-        try {
-            List<Member> result = em.createQuery("select m from Member", Member.class).getResultList();
-
-            for(Member member : result){
-                System.out.println("member.name = " + member.getName());
-            }
-
-            tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-        } finally {
-            em.close();
-        }
-
+        em.close();
         emf.close();
     }
 }
